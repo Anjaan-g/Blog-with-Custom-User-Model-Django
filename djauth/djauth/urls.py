7b9e43a4588f4dda23613ce7f0d26132951e1574
Anjaan <anjaangaire@gmail.com>
2019-05-21 21:47:50 +0545

    Blog app created.

--- djauth/djauth/urls.py
+++ djauth/djauth/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from blog.views import (
+    blog_post_create_view,
+)
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
+    path('blog/', include('blog.urls')),
+    path('blog-new/',blog_post_create_view),
 ]
